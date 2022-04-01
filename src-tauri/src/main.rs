33d3c33a475d0dbae95a4e2232a53ea056caf912
puuuii <rx4_raffin@yahoo.co.jp>
2022-04-01 22:18:41 +0900

    init: 一度作り直し、音声情報を取得して表示するところまで

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,32 +3,25 @@
   windows_subsystem = "windows"
 )]
 
-use std::fs::File;
-use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source};
+use std::fs;
+use std::path::Path;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![test_play])
+    .invoke_handler(tauri::generate_handler![get_audio_list])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn test_play() {
-  // Get a output stream handle to the default physical sound device
-  let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-  // Load a sound from a file, using a path relative to Cargo.toml
-  let file = BufReader::new(File::open("F:/kindletext/audio/世界史.flac").unwrap());
-  // Decode that sound file into a source
-  let source = Decoder::new(file).unwrap();
-  // Play the sound directly on the device
-  match stream_handle.play_raw(source.convert_samples()) {
-    Ok(_) => {
-      // The sound plays in a separate audio thread,
-      // so we need to keep the main thread alive while it's playing.
-      std::thread::sleep(std::time::Duration::from_secs(5));
-    },
-    Err(e) => println!("{:?}", e),
-  }
+fn get_audio_list(path: &Path) -> Vec<(String, f32)> {
+    fs::read_dir(path).unwrap().filter_map(|entry| {
+        let entry = entry.ok()?;
+        if entry.file_type().ok()?.is_file() {
+            Some((entry.file_name().to_string_lossy().into_owned(), 35.3))
+        } else {
+            None
+        }
+    })
+    .collect()
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,42 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-  import { invoke } from '@tauri-apps/api/tauri'
+  import { invoke, convertFileSrc } from '@tauri-apps/api/tauri'
+	import UIkit from 'uikit'
+	import Icons from 'uikit/dist/js/uikit-icons'
+	import 'uikit/dist/css/uikit.css'
+	import 'uikit/dist/css/uikit.min.css'
 
-  invoke('test_play_')
-		.then((msg) => {
-      console.log()
-		})
+	UIkit.use(Icons);
+
+  const dir = 'F:/kindletext/audio/';
+
+  let info = {};
+  invoke('get_audio_list', {path: dir})
+  .then((l) => Object.values(l).forEach(item => info[item[0]] = item[1]));
+
+  const record = (e) => {
+    const target = e.target;
+    console.log(target.currentTime);
+    console.log(decodeURI(target.src.split('/').pop()));
+  };
+
+  const set_current_time = (e) => {
+    const target = e.target;
+    const time = info[decodeURI(target.src.split('/').pop())]
+    target.currentTime = time;
+  };
 </script>
 
 <main>
-  <h1>hello</h1>
+  <div class="uk-container uk-container-xsmall">
+    <ul class="uk-list uk-list-divider">
+      {#each Object.entries(info) as [name, time]}
+        <li>
+          <audio class="tmp" controls src={convertFileSrc(dir)+name} on:loadeddata="{set_current_time}" on:pause={record}/>
+          {name}
+        </li>
+      {/each}
+    </ul>
+  </div>
 </main>
 
 <style lang="scss">
